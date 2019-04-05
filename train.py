8ad2ee1b20d3e6231837bc85bb2bf1816cd98787
Jiaqi Huang <huangjiaqi88@hotmail.com>
2019-04-05 11:38:12 -0700

    JH: fix

--- train.py
+++ train.py
@@ -11,40 +11,43 @@ from sklearn.model_selection import train_test_split, cross_val_score, cross_val
 import os
 from imblearn.under_sampling import NearMiss
 from imblearn.metrics import classification_report_imbalanced
-from sklearn.linear_model import Lasso, LogisticRegression, SGDClassifier, SGDRegressor,Perceptron
+from sklearn.linear_model import Lasso, LogisticRegression, SGDClassifier, SGDRegressor, Perceptron
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, mean_squared_error, mean_absolute_error, explained_variance_score,r2_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, mean_squared_error, mean_absolute_error, explained_variance_score, r2_score
 from sklearn.naive_bayes import GaussianNB
 import matplotlib.pyplot as plt
+
 
 def get_models_names(multiclass_opt='multinomial'):
     models = [
         DecisionTreeClassifier(),
-       RandomForestClassifier(n_estimators=200),
+        RandomForestClassifier(n_estimators=200),
         LogisticRegression(
             solver='lbfgs',
             multi_class=multiclass_opt,
             max_iter=3000
         ),
-       SGDClassifier(max_iter=10000, alpha=0.00001,tol=1e-3)
+        SGDClassifier(max_iter=10000, alpha=0.00001, tol=1e-3)
     ]
 
     names = [
-          "Decision Tree",
-           "RandomForestClassifier",
-             "LogisticRegression",
-             "SGDClassifier"
-            ]
-    
+        "Decision Tree",
+        "RandomForestClassifier",
+        "LogisticRegression",
+        "SGDClassifier"
+    ]
+
     return models, names
+
 
 def getScores(estimator, x, y):
     yPred = estimator.predict(x)
     return (accuracy_score(y, yPred),
             precision_score(y, yPred, average='macro'),
             recall_score(y, yPred, average='macro'))
+
 
 def my_scorer(estimator, x, y):
     a, p, r = getScores(estimator, x, y)
@@ -60,30 +63,31 @@ def train_status(config, x, y, test_x, test_y):
     print('Resampled dataset shape %s' % Counter(y))
     print(x.shape)
     models, names = get_models_names()
-   
 
     for model, name in zip(models, names):
         print(name)
         start = time.time()
-        feature_names = ["station_id", "hour", "avg(temp)", "avg(wind)", "weekno"]
+        feature_names = ["station_id", "hour",
+                         "avg(temp)", "avg(wind)", "weekno"]
 
         if name == 'LogisticRegression':
-            model = model.fit(x,y)
-            train_score = model.score(x,y)
+            model = model.fit(x, y)
+            train_score = model.score(x, y)
             print(train_score)
             print("test")
-            score = model.score(test_x,test_y)
+            score = model.score(test_x, test_y)
             accuracy = accuracy_score(test_y, model.predict(test_x))
             print(score)
             print(accuracy)
             print('time', time.time() - start, '\n\n')
         else:
-            m = cross_validate(model, x, y, scoring=my_scorer, cv=15, return_estimator=True)
+            m = cross_validate(model, x, y, scoring=my_scorer,
+                               cv=15, return_estimator=True)
             model = m['estimator'][-1]
             model = model.fit(x, y)
 
            #  model = model.fit(x, y)
-   
+
             # for y_, predY_ in zip(test_y,yPred):
             #     print("truth lable: %d pred: %d" % (y_, predY_))
             # print(y1)
@@ -105,16 +109,18 @@ def train_status(config, x, y, test_x, test_y):
             print("Feature ranking:")
 
             for f in range(x.shape[1]):
-                print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
-            feature_names = ["station_id", "hour", "month","avg(temp)", "avg(wind)", "weekno"]
+                print("%d. feature %d (%f)" %
+                      (f + 1, indices[f], importances[indices[f]]))
+            feature_names = ["station_id", "hour", "month",
+                             "avg(temp)", "avg(wind)", "weekno"]
             fn = rearrange_feature(feature_names, indices)
 
             # Plot the feature importances of the forest
             plt.figure()
             plt.title("Feature importances")
-            c = np.random.rand() 
+            c = np.random.rand()
             plt.bar(range(x.shape[1]), importances[indices],
-                color='#4682B4', align="center")
+                    color='#4682B4', align="center")
             plt.xticks(range(x.shape[1]),  fn)
             plt.xlim([-1, x.shape[1]])
             plt.show()
@@ -148,6 +154,7 @@ def rearrange_feature(feature_names, index):
         f.append(feature_names[i])
     return f
 
+
 def train_difference(config, x, y, test_x, test_y):
     print("...start training difference model....")
     print('Original dataset shape %s' % Counter(y))
@@ -156,20 +163,21 @@ def train_difference(config, x, y, test_x, test_y):
     print('Resampled dataset shape %s' % Counter(y))
     print(x.shape)
 
-    models,names = get_models_names(multiclass_opt='ovr')
+    models, names = get_models_names(multiclass_opt='ovr')
     for model, name in zip(models, names):
         print(name)
         start = time.time()
 
         if name == 'LogisticRegression':
             # m = np.mean(cross_val_score(model, x, y, scoring=model.score, cv=5))
-            model = model.fit(x,y)
+            model = model.fit(x, y)
             print("test")
-            score = model.score(test_x,test_y)
+            score = model.score(test_x, test_y)
             print(score)
             print('time', time.time() - start, '\n\n')
         else:
-            m = cross_validate(model, x, y, scoring=my_scorer, cv=15, return_estimator=True)
+            m = cross_validate(model, x, y, scoring=my_scorer,
+                               cv=15, return_estimator=True)
             model = m['estimator'][-1]
             model = model.fit(x, y)
             print("test")
@@ -210,7 +218,7 @@ def train_difference(config, x, y, test_x, test_y):
             # # Plot the feature importances of the forest
             # plt.figure()
             # plt.title("Feature importances")
-            # c = np.random.rand() 
+            # c = np.random.rand()
             # plt.bar(range(x.shape[1]), importances[indices],
             #     color='#afeeee', align="center")
             # plt.xticks(range(x.shape[1]),  fn)
@@ -221,8 +229,5 @@ def train_difference(config, x, y, test_x, test_y):
         # print("test")
         # scores = my_scorer(model, test_x, test_y)
         # print('time', time.time() - start, '\n\n')
-      
 
         # scores = my_scorer(model, test_x, test_y)
-        
-
